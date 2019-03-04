78994f50ba9737deff507cee14aab6cee37c8a9b
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-03-04 17:02:51 -0500

    working on register post

--- routers/auth.js
+++ routers/auth.js
@@ -6,4 +6,21 @@ const knexConfig = require('../knexfile.js')
 const db = knex(knexConfig.development);
 
 
+router.post('/api/register', (req, res) => {
+    let user = req.body;
+  
+    const hash = bcrypt.hashSync(user.password, 12)
+    user.password = hash
+  
+    db.insert(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -8,8 +8,13 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 
-server.use('/api/register', authRouter);
-server.use('/api/login', authRouter);
+// server.use('/api/register', authRouter);
+// server.use('/api/login', authRouter);
+
+server.get('/', (req, res) => {
+    res.send("Auth Auth Auth!");
+  });
 
 module.exports = server;
