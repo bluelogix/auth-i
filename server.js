0fb3a73cc8e219af7ffd28d730d480c80c651185
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-03-04 16:14:39 -0500

    added knexfile and migration

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const authRouter = require('./routers/auth.js');
+// const authRouter = require('./routers/auth.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.use('/api/register', authRouter);
+// server.use('/api/register', authRouter);
 
 module.exports = server;
