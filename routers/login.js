04ad9ae25546298397c820db69fdc12e00f985c6
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-03-05 17:13:32 -0500

    added sessions to the login, functioning. now working on log out section

--- routers/login.js
+++ routers/login.js
@@ -1,11 +1,28 @@
 const loginRouter = require('express').Router();
+const logoutRouter = require('express').Router();
 
 const bcrypt = require('bcryptjs');
+const session = require('express-session'); 
 
 const db = require('./config.js');
 const Users = require('./users-model.js');
 
 
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 60 * 60, // in ms
+    secure: false, // used over https only
+  },
+  httpOnly: true, // cannot access the cookie from js using document.cookie
+  resave: false,
+  saveUninitialized: false, // GDPR laws against setting cookies automatically
+  
+};
+
+
+loginRouter.use(session(sessionConfig)); 
 // LOGIN USER 
 
 loginRouter.post('/', (req, res) => {
@@ -15,6 +32,7 @@ loginRouter.post('/', (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.user = user
           res.status(200).json({ message: `You are logged in ${user.username}!` });
         } else {
           res.status(401).json({ message: 'You shall not pass!' });
@@ -25,7 +43,21 @@ loginRouter.post('/', (req, res) => {
       });
   });
 
+  logoutRouter.get('/api/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send(
+            'Cannot sign user out'
+          );
+        } else {
+          res.send('Successfully signed out!');
+        }
+      });
+    } else {
+      res.end();
+    }
+  });
 
 
-
-module.exports = loginRouter;
+module.exports = loginRouter, logoutRouter;
